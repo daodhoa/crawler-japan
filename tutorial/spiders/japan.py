8255d3b15b88ebd6d1b62ef807fcaf7e31cbf0b4
h0adaynd <hoadd1@hblab.vn>
2020-03-18 09:36:30 +0700

    multi page

--- tutorial/spiders/japan.py
+++ tutorial/spiders/japan.py
@@ -3,9 +3,17 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "japan"
-    start_urls = ['#']
+    start = 1
+    end = 10
+
+    def start_requests(self):
+        main_url = 'https://japanesetest4you.com/japanese-language-proficiency-test-jlpt-n5-vocabulary-exercise-'
+        for i in range(self.start, self.end + 1):
+            url = main_url + str(i) + '/'
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
+        page_number = response.url.split('-')[-1].replace('/', '');
         tests = response.css('form p')
         page_tests = dict()
         for test in tests:
@@ -15,12 +23,11 @@ class QuotesSpider(scrapy.Spider):
             answers = filter(lambda x: x != "", answers)
             answers = list(answers)
             if answers:
-                page_tests[number] = dict(number=number, question=question, answers = answers)
+                page_tests[str(page_number) + '.' + str(number)] = dict(number=number, question=question, answers = answers)
 
-        #yield page_tests
         corrects = response.css('body').re(r'Question (\w+: \w+)')
         for correct in corrects:
             index = correct[:2].replace(':', '')
             ans = correct[-1]
-            page_tests[index]['correct'] = ans
+            page_tests[str(page_number) + '.' + str(index)]['correct'] = ans
         yield page_tests
